cd02c46b3959705f03b38132abe378deb8e37e6e
celine21-meet <celine21@meet.mit.edu>
2019-07-22 03:00:14 -0400

    Stopping here

---
+++ lab_2.py
@@ -0,0 +1,44 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======== RESTART: /home/student/celine21_lab2/meet2019y1lab2/lab_2.py ========
+>>> 
+======== RESTART: /home/student/celine21_lab2/meet2019y1lab2/lab_2.py ========
+>>> type(259+33)
+<class 'int'>
+>>> lab_2.py
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    lab_2.py
+NameError: name 'lab_2' is not defined
+>>> type(259-33.0)
+<class 'float'>
+>>> type(4)
+<class 'int'>
+>>> type('4')
+<class 'str'>
+>>> type('four')
+<class 'str'>
+>>> type(12 > 2*5)
+<class 'bool'>
+>>> type(5/2.0)
+<class 'float'>
+>>> type(color+3)
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    type(color+3)
+NameError: name 'color' is not defined
+>>> type('color'*4)
+<class 'str'>
+>>> type('color'+'3')
+<class 'str'>
+>>> ('4')
+'4'
+>>> (5/2.0)
+2.5
+>>> type('color'*4)
+<class 'str'>
+>>> print('color'*4)
+colorcolorcolorcolor
+>>> 
